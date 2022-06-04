2f1da96443b53fbd175a2a7f46917c56c1a484b8
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2022-06-03 23:36:31 -0400

    Switch from Vaadin to css, html, and js

---
+++ src/main/webapp/js/dashboard.js
@@ -0,0 +1,84 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+    console.log("hii");
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+
+const input = document.querySelector("#input");
+
+input.addEventListener('input', () => {
+    const cameras = document.getElementsByClassName('camera');
+    for (var i = 0; i < cameras.length; i++) {
+        if (!(cameras[i].id.toLowerCase().includes(input.value))) {
+            cameras[i].style.display = 'none';
+        } else {
+            cameras[i].style.display = 'block';
+        }
+    }
+});
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+var light = true;
+const themeToggler = document.querySelector('.theme-Toggler');
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    light = !light;
+    let newInner = `<span
+					class="material-icons">light_mode</span>
+					<h3>Toggle Mode</h3>`;
+    if (light) {
+      	newInner = `<span
+					class="material-icons">dark_mode</span>
+					<h3>Toggle Mode</h3>`;
+    }
+    themeToggler.innerHTML = newInner;
+});
+
+window.addEventListener('resize', () => {
+    if (window.outerWidth >= 769) {
+        sideMenu.style.display = 'block';
+    }
+});
